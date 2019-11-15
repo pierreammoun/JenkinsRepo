a428174a8b78b70a47541655ede88f7caf2ff91c
Pierre Ammoun <pierre@pcs-lb.com>
2019-11-15 11:42:34 +0200

    this will fail

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,7 @@ public class HelloWorld {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
 	System.out.println("Hello, Jenkins");
+asdf
     }
 
 }
